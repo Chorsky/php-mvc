8c109ae0a3e3a28e2c9edf7c76abbf7a1f67d6bc
Cesar <cesargouvea@gmail.com>
2023-01-31 17:09:35 -0300

    Primeiro commit de arquivos

---
+++ app/Model/Entity/Organization.php
@@ -0,0 +1,32 @@
+<?php
+namespace App\Model\Entity;
+
+class Organization 
+{
+  /**
+   * ID da organização
+   * @var integer
+  */
+  public $id = 1;
+
+  /**
+   * Nome da organização
+   * @var string
+  */
+  public $name = 'Evernet';
+
+  /**
+   * Site da organização
+   * @var string
+  */
+  public $site = 'https://www.evernet.com.br/';
+
+  /**
+   * Descrição da organização
+   * @var string
+  */
+  public $description = 'Aliquip eiusmod labore proident quis est nulla ex incididunt do sunt aliquip. Id Lorem aliquip labore irure occaecat adipisicing. Commodo est exercitation anim occaecat incididunt pariatur aliqua labore cillum laboris sint. Labore exercitation irure enim labore culpa laborum esse ad irure deserunt.';
+
+  
+
+}
